22781d6a3936c5ee6795af34f2cf4c0e4650ea1a
devintheamateurdevin <devintheamateurdevin@gmail.com>
2022-09-03 20:46:43 +0300

    Progress on organizing routes

---
+++ src/router/private/index.js
@@ -0,0 +1,17 @@
+import HomePage from "@/views/HomePage.vue";
+import UserLogin from "@/views/auth/UserLogin.vue";
+import UserSignUp from "@/views/auth/UserSignUp.vue";
+import ForgotPassword from "@/views/auth/ForgotPassword.vue";
+
+export const privateRoutes = [
+    {path: "/", redirect: {name: "home"}, name: "root", meta: {requiresAuth: false, showFooter: true}},
+    {path: "/home", component: HomePage, name: "home", meta: {requiresAuth: false, showFooter: true}},
+    {path: "/login", component: UserLogin, name: "login", meta: {requiresAuth: false, showFooter: true}},
+    {path: "/sign-up", component: UserSignUp, name: "sign-up", meta: {requiresAuth: false, showFooter: true}},
+    {
+        path: "/forgot-password",
+        component: ForgotPassword,
+        name: "forgot-password",
+        meta: {requiresAuth: false, showFooter: true}
+    },
+]
