c97129f563b627a6c10be4f9fb45d197d8f4b797
devintheamateurdevin <devintheamateurdevin@gmail.com>
2023-08-22 09:38:36 +0300

    Adds basic footer

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,22 +1,47 @@
 <template>
   <footer>
-    <p>This is a footer</p>
-    <nav>
+    <nav class="page-links">
       <ul>
-        <li><a></a></li>
+      </ul>
+    </nav>
+
+
+    <nav class="social-links">
+      <ul>
+        <li><a href="https://github.com/squivix/dzelda-frontend" target="_blank">
+          <img :src="assets.githubLogo" alt="github link">
+        </a></li>
       </ul>
     </nav>
   </footer>
 </template>
 <script lang="ts">
-  export default {
-    name: "TheFooter",
-    components: {},
+import githubLogo from "@/assets/images/github-logo.svg";
+
+export default {
+  name: "TheFooter",
+  components: {},
+  setup() {
+    return {assets: {githubLogo}};
   }
+};
 </script>
 <style scoped>
 footer {
-  background-color: lightgray;
-  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  background-color: #F2F2F2;
+}
+
+.social-links ul {
+  display: flex;
+  column-gap: 1rem;
+}
+
+.social-links img {
+  width: 25px;
+  height: 25px;
 }
 </style>
