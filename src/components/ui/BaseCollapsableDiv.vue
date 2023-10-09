afbf8c0e71d22f720d86fa953801ad67e16a50e9
devintheamateurdevin <devintheamateurdevin@gmail.com>
2023-10-09 13:29:24 +0300

    adds base collapsable div component

---
+++ src/components/ui/BaseCollapsableDiv.vue
@@ -0,0 +1,49 @@
+<template>
+  <div :class="{'collapsable-div':true, 'is-hidden':!isShown}"
+       @transitionstart="onTransitionChanged(true)"
+       @transitionend="onTransitionChanged(false)">
+    <slot class="contents">
+
+    </slot>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "BaseCollapsableDiv",
+  emits: ["onTransitionChanged"],
+  props: {
+    isShown: {type: Boolean, required: true},
+    toggleElementRef: {type: HTMLElement}
+  },
+  methods: {
+    onTransitionChanged(isTransitioning: boolean) {
+      this.$emit("onTransitionChanged", isTransitioning);
+    }
+  }
+});
+</script>
+
+<style scoped>
+
+.collapsable-div {
+  max-height: 1000px;
+  overflow: hidden;
+  padding: 1rem;
+  transition-property: max-height, padding;
+  transition-duration: 0.5s;
+}
+
+.collapsable-div.is-hidden {
+  max-height: 0;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+
+.collapsable-div.is-hidden :deep(*) {
+  pointer-events: none;
+}
+
+</style>
