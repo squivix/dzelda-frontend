044d1bb50c2f2ccafce5432258066f6c500077b1
devintheamateurdevin <devintheamateurdevin@gmail.com>
2024-02-13 17:19:27 +0300

    changes page indicator to divided bar to look better with long lessons

--- src/components/page/reader/PageIndicator.vue
+++ src/components/page/reader/PageIndicator.vue
@@ -1,14 +1,6 @@
 <template>
-  <div class="page-indicators">
-    <button v-if="pages[0]!=1" class="start-end-button inv-button" @click="$emit('onPageIndicatorClicked', 1)">
-      <inline-svg :src="icons.doubleArrowLeft"/>
-    </button>
-    <button v-for="p in pages" class="inv-button" @click="$emit('onPageIndicatorClicked', p)">
-      <div :class="{indicator:true, 'current':p==currentPage, 'hide':pageCount<2}"></div>
-    </button>
-    <button v-if="pages[pages.length-1]!=pageCount" class="start-end-button inv-button" @click="$emit('onPageIndicatorClicked', pageCount)">
-      <inline-svg :src="icons.doubleArrowRight"/>
-    </button>
+  <div class="bar">
+    <div v-for="page in  pageCount" :class="{'section-indicator':true,  'current-section-indicator':page<=currentPage}" @click="$emit('onPageIndicatorClicked', page)"></div>
   </div>
 </template>
 
@@ -23,23 +15,6 @@ export default defineComponent({
   props: {
     currentPage: {type: Number, required: true},
     pageCount: {type: Number, required: true},
-    indicatorLimit: {type: Number, default: 30},
-  },
-  data() {
-    return {
-      beforeCurrentCount: 10,
-      afterCurrentCount: 10,
-    };
-  },
-  computed: {
-    pages() {
-      let pages = [];
-      let start = Math.max(1, this.currentPage - this.beforeCurrentCount);
-      let end = Math.min(this.pageCount + 1, start + Math.min(this.beforeCurrentCount + this.afterCurrentCount + 1, this.pageCount));
-      for (let i = start; i < end; i++)
-        pages.push(i);
-      return pages;
-    },
   },
   setup() {
     return {icons};
@@ -48,44 +23,24 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.page-indicators {
+.bar {
+  width: 80%;
+  margin-bottom: 0.5rem;
   display: flex;
-  flex-wrap: wrap;
-  align-items: center;
+  justify-content: space-evenly;
+  column-gap: 0.5rem;
 }
 
-button {
-  padding: 4px;
-}
-
-.indicator {
-  width: 12px;
-  height: 12px;
+.section-indicator {
   background-color: lightgray;
-  border-radius: 50%;
-}
-
-.indicator:hover {
+  height: 0.5rem;
+  flex-grow: 1;
   cursor: pointer;
 }
 
-.indicator.current {
+.current-section-indicator {
   background-color: var(--secondary-color);
 }
 
-.start-end-button {
-  display: grid;
-  place-items: center;
-}
 
-.start-end-button svg {
-  width: 12px;
-  height: 12px;
-  color: var(--secondary-color);
-}
-
-.hide {
-  visibility: hidden;
-  pointer-events: none;
-}
 </style>
